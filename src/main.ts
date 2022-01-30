f3f549ac0af8699ec0a73d1b8fd1af79b4baeab2
Kacper Salek <kacper.salek32@gmail.com>
2022-01-30 13:56:32 +0100

    create bootstrap function to run nest app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -12,3 +13,10 @@ class AppController {
   controllers: [AppController],
 })
 class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+bootstrap();
